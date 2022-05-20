cfb02499ae99d7475532f8ff2e0b37e76acae7e6
Patryk Zadroga <pzadroga@thegc.com>
2022-05-20 13:26:29 +0200

    Connection with struct and real value achived

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -43,9 +43,10 @@ namespace Beckhoff_VS_Visualisation
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(17, 486);
+            this.textBox1.Location = new System.Drawing.Point(23, 598);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.Size = new System.Drawing.Size(132, 22);
             this.textBox1.TabIndex = 0;
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
@@ -59,17 +60,19 @@ namespace Beckhoff_VS_Visualisation
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label1.Location = new System.Drawing.Point(252, 9);
+            this.label1.Location = new System.Drawing.Point(336, 11);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(70, 26);
+            this.label1.Size = new System.Drawing.Size(86, 31);
             this.label1.TabIndex = 1;
             this.label1.Text = "label1";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(17, 349);
+            this.button1.Location = new System.Drawing.Point(23, 430);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(100, 28);
             this.button1.TabIndex = 2;
             this.button1.Text = "Test ON";
             this.button1.UseVisualStyleBackColor = true;
@@ -77,9 +80,10 @@ namespace Beckhoff_VS_Visualisation
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(17, 401);
+            this.button2.Location = new System.Drawing.Point(23, 494);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(100, 28);
             this.button2.TabIndex = 3;
             this.button2.Text = "Test OFF";
             this.button2.UseVisualStyleBackColor = true;
@@ -89,9 +93,10 @@ namespace Beckhoff_VS_Visualisation
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label2.Location = new System.Drawing.Point(22, 9);
+            this.label2.Location = new System.Drawing.Point(29, 11);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(70, 26);
+            this.label2.Size = new System.Drawing.Size(86, 31);
             this.label2.TabIndex = 4;
             this.label2.Text = "label2";
             // 
@@ -100,37 +105,39 @@ namespace Beckhoff_VS_Visualisation
             this.label3.AutoSize = true;
             this.label3.BackColor = System.Drawing.SystemColors.Highlight;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label3.Location = new System.Drawing.Point(22, 69);
+            this.label3.Location = new System.Drawing.Point(29, 85);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(127, 26);
+            this.label3.Size = new System.Drawing.Size(155, 31);
             this.label3.TabIndex = 5;
             this.label3.Text = "Actual BPM";
             // 
             // label4
             // 
-            this.label4.AutoSize = true;
             this.label4.BackColor = System.Drawing.SystemColors.ButtonHighlight;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label4.Location = new System.Drawing.Point(22, 95);
+            this.label4.Location = new System.Drawing.Point(29, 117);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(127, 26);
+            this.label4.Size = new System.Drawing.Size(155, 48);
             this.label4.TabIndex = 6;
             this.label4.Text = "Actual BPM";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(273, 150);
+            this.label5.Location = new System.Drawing.Point(364, 185);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.Size = new System.Drawing.Size(46, 17);
             this.label5.TabIndex = 7;
             this.label5.Text = "label5";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1904, 1041);
+            this.ClientSize = new System.Drawing.Size(2539, 1281);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
@@ -139,6 +146,7 @@ namespace Beckhoff_VS_Visualisation
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Vision System";
             this.Load += new System.EventHandler(this.Form1_Load);
--- Form1.cs
+++ Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Reactive.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -18,13 +19,16 @@ namespace Beckhoff_VS_Visualisation
 {
     public partial class Form1 : Form
     {
-        int ADS_Port = 851;
+        //AmsPort ADS_Port = 851;
+        AmsNetId AMS_NetID;
+        int i=0;
+        Single j = 0;
+
         public Form1()
         {
             InitializeComponent();
         }
         private AdsClient ads = new AdsClient();
-        bool testVarRead;
         public struct ST_DeviceCtrl
         {
             public int iBPM { get; }
@@ -34,7 +38,6 @@ namespace Beckhoff_VS_Visualisation
         private void Form1_Load(object sender, EventArgs e)
         {
             CheckConnectionWithLocalPLC();
-
         }
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
@@ -46,18 +49,49 @@ namespace Beckhoff_VS_Visualisation
             CheckConnectionWithLocalPLC();
             DateTime dt;
             dt = DateTime.Now;
-            textBox1.Text = ((Int32)ads.ReadAny(ads.CreateVariableHandle("prgAlarms.HMI_Alarms_Counter"), typeof(Int32))).ToString();
-            label2.Text = dt.ToString();
-            //Dwie linijki poniżej są błędna ponieważ nie potrafię odczytać wartości globalnych
-            label5.Text = ((Single)ads.ReadAny(ads.CreateVariableHandle("Global_Variables.rEff1"), typeof(Single))).ToString();
-            label4.Text = ((int)ads.ReadAny(ads.CreateVariableHandle("Global_Variables.stDeviceCtrl.iBPM"), typeof(ST_DeviceCtrl))).ToString();
+            try
+            {
+                textBox1.Text = ((Int32)ads.ReadAny(ads.CreateVariableHandle("prgAlarms.HMI_Alarms_Counter"), typeof(Int32))).ToString();
+                label2.Text = dt.ToString();
+                // Create Symbol information (Symbol 'i : INT' in PLC Global Variables list.
+                var symbolLoader = TwinCAT.Ads.TypeSystem.SymbolLoaderFactory.Create(ads, TwinCAT.SymbolLoaderSettings.Default);
+                TwinCAT.TypeSystem.IValueSymbol gvlIntSymbol = (TwinCAT.TypeSystem.IValueSymbol)symbolLoader.Symbols["GVL.i"];
+                i++;
+                // Take 10 Values (0..9) and write them to GVL.i
+                gvlIntSymbol.WriteValue(i);
 
+                label5.Text = ((Single)ads.ReadAny(ads.CreateVariableHandle(".rEff1"), typeof(Single))).ToString();
+                label4.Text = ((int)ads.ReadAny(ads.CreateVariableHandle(".stDeviceCtrl.iBPM"), typeof(int))).ToString();
+            }
+            catch (Exception error1)
+            {
+                timer1.Enabled = false;
+                // Initializes the variables to pass to the MessageBox.Show method.
+                string message = "Application can not read PLC Variables \nYes to Close App \nNo to try again\nMessage Below :\n" + error1;
+                string caption = "Error Detected in Input";
+                MessageBoxButtons buttons = MessageBoxButtons.YesNo;
+                DialogResult result;
+
+                // Displays the MessageBox.
+                result = MessageBox.Show(message, caption, buttons);
+                if (result == System.Windows.Forms.DialogResult.Yes)
+                {
+                    // Closes the parent form.
+                    this.Close();
+                }
+                if (result == System.Windows.Forms.DialogResult.No)
+                {
+                    // Closes the parent form.
+                    timer1.Enabled = true;
+                }
+            }
         }
         private void button1_Click(object sender, EventArgs e)
         {
-            ads.WriteAny(ads.CreateVariableHandle("prgAlarms.test");, true);
+            bool testVarRead;
+            ads.WriteAny(ads.CreateVariableHandle("prgAlarms.test"), true);
 
-            testVarRead = (bool)ads.ReadAny(ads.CreateVariableHandle("prgAlarms.test");, typeof(bool));
+            testVarRead = (bool)ads.ReadAny(ads.CreateVariableHandle("prgAlarms.test"), typeof(bool));
             if (testVarRead)
             {
 
@@ -68,9 +102,10 @@ namespace Beckhoff_VS_Visualisation
 
         private void button2_Click(object sender, EventArgs e)
         {
-            ads.WriteAny(ads.CreateVariableHandle("prgAlarms.test");, false);
+            bool testVarRead;
+            ads.WriteAny(ads.CreateVariableHandle("prgAlarms.test"), false);
             
-            testVarRead = (bool)ads.ReadAny(ads.CreateVariableHandle("prgAlarms.test");, typeof(bool));
+            testVarRead = (bool)ads.ReadAny(ads.CreateVariableHandle("prgAlarms.test"), typeof(bool));
             if (!testVarRead)
             {
                 button2.BackColor = Color.Green;
@@ -83,10 +118,11 @@ namespace Beckhoff_VS_Visualisation
             try
             {
                 // Connect to local TwinCAT System Service
-                ads.Connect(ADS_Port);
+                //ads.Connect(ADS_Port);
+                ads.Connect(AmsNetId.Local, 851);
                 if (ads.IsConnected == true)
                 {
-                    label1.Text = ("Connection OK");
+                    label1.Text = ("Connection with PLC OK");
                     label1.BackColor = Color.Green;
                 }
                 else
@@ -102,3 +138,10 @@ namespace Beckhoff_VS_Visualisation
         }
     }
 }
+
+//Different Type of Reading Connecting to Variable in PLC
+//var symbolLoader1 = TwinCAT.Ads.TypeSystem.SymbolLoaderFactory.Create(ads, TwinCAT.SymbolLoaderSettings.Default);
+//TwinCAT.TypeSystem.IValueSymbol gvlRealSymbol = (TwinCAT.TypeSystem.IValueSymbol)symbolLoader1.Symbols["Global_Variables.rEff1"];
+//j++;
+//// Take 10 Values (0..9) and write them to GVL.i
+//gvlRealSymbol.WriteValue(j);
